e8d0e83a3572f3e45c2a6d07479d04f7c7614adb
gitsubash44 <dhamisubash866@gmail.com>
2024-12-22 19:03:47 +0545

    first commit

---
+++ healthy_sabji/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for healthy_sabji project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from sabji import views
+
+urlpatterns = [
+    # path for Users Dashboard
+    path('admin/', admin.site.urls),
+    path('',views.index,name="index"),
+    path('about/',views.about,name="about"),
+    path('product/',views.product,name="product"),
+    path('product_desc/',views.productDetail,name="productDetail"),
+    path('cart/',views.cart,name="cart"),
+    path('checkout/',views.checkout,name="checkout"),
+    path('contact/',views.contact,name="contact"),
+    path('user_profile/',views.user_profile,name="user_profile"),
+    path('orderhistory/',views.orderhistory,name="orderhistory"),
+    path('all_order_history/',views.all_order_history,name="all_order_history"),
+    path('order_track/',views.order_track,name="order_track"),
+    
+    # Path for Delivery Dashboard
+    path('delivery_dashboard/',views.delivery_dashboard,name="delivery_dashboard"),
+    
+    #Path for Farmer Dashboard
+    path('farmer_dashboard/',views.farmer_dashboard,name="farmer_dashboard"),
+]
---
+++ sabji/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# For User views Site.
+def index(request):
+    return render(request, 'user/index.html')
+
+
+def about(request):
+    return render(request,'user/about.html')
+
+def product(request):
+    return render(request, 'user/product.html')
+
+
+def productDetail(request):
+    return render(request,'user/product_desc.html')
+
+def cart(request):
+    return render(request,'user/cart.html')
+
+def checkout(request):
+    return render(request,'user/checkout.html')
+
+def contact(request):
+    return render(request,'user/contact.html')
+
+def user_profile(request):
+    return render(request,'user/user_profile.html')
+
+def orderhistory(request):
+    return render(request,'user/orderhistory.html')
+
+def all_order_history(request):
+    return render(request,'user/all_order_history.html')
+
+def order_track(request):
+    return render(request,'user/order_track.html')
+
+
+# For delivery Site.
+def delivery_dashboard(request):
+    return render(request,'delivery/delivery_dashboard.html')
+
+# for Farmer site.
+def farmer_dashboard(request):
+    return render(request,'farmer/farmer_dashboard.html')
